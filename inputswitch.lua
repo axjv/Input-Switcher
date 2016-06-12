58670684a39564ca8fd43098f552d449bee56b92
axjv <a.vienma@gmail.com>
2016-06-13 03:57:16 +0700

    Add toggle, cleanup code

--- inputswitch.lua
+++ inputswitch.lua
@@ -1,36 +1,29 @@
 local acutil = require('acutil');
 local loaded = 0
-function INPUTSWITCH_ON_INIT(addon, frame)
-	if (loaded == 0) then
-		acutil.slashCommand('/inputswitch',parse);
-		ui.SysMsg("Input switcher loaded! To use, type /inputswitch.")
-		loaded = 1
-	end
-end 
-function INPUTSWITCH(x)
-	if (x == 0) then
-		config.ChangeXMLConfig("ControlMode", 2);
-		session.config.SetMouseMode(false);
-		UPDATE_CONTROL_MODE()
-	elseif (x == 1) then 
-		config.ChangeXMLConfig("ControlMode", 3);
-		session.config.SetMouseMode(true);
-		UPDATE_CONTROL_MODE()
-	end
-	return 0;
-end
+
+acutil.slashCommand('/inputswitch',parse);
+ui.SysMsg("Input switcher loaded! To use, type /inputswitch.")
+
 
 function parse(command)
 	local cmd = table.remove(command,1);
 	if (cmd == 'kb') then
-		INPUTSWITCH(0)
+		config.ChangeXMLConfig("ControlMode", 2);
 		return ui.SysMsg('Keyboard mode enabled.');
 	end
 	if (cmd == 'mouse') then
-		INPUTSWITCH(1)
+		config.ChangeXMLConfig("ControlMode", 3);
+		return ui.SysMsg('Mouse mode enabled.')
+	end
+	if (cmd == 'toggle') then
+		if config.GetXMLConfig("ControlMode") == 3 then
+			config.ChangeXMLConfig("ControlMode", 2)
+			return ui.SysMsg('Keyboard mode enabled.')
+		end
+		config.ChangeXMLConfig("ControlMode", 3)
 		return ui.SysMsg('Mouse mode enabled.')
 	end
 	if (not cmd) then
-		return ui.SysMsg('Type /inputswitch kb to enable keyboard, or /inputswitch mouse to enable mouse.')
+		return ui.SysMsg('Type /inputswitch kb to enable keyboard, /inputswitch mouse to enable mouse, or /inputswitch toggle to toggle.')
 	end
 end
